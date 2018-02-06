465e99e93585d79c46e301a4f6c1bbb07cd377b5
Sarah Fava <sarahfava12@gmail.com>
2018-02-06 21:28:19 +0000

    Add colors to geometric_pattern.

--- Geopattern/main.js
+++ Geopattern/main.js
@@ -1,6 +1,9 @@
 var NUM_CIRCLES = 12;
 var circleDiameter;
 var circleRadius;
+var rVal;
+var gVal;
+var bVal;
 
 function setup() {
     createCanvas(480, 600);
@@ -9,6 +12,10 @@ function setup() {
  }
  
 function draw() {
+  rVal = 255;
+  gVal = 0;
+  bVal = 0;
+  
     var isShifted = false;
     
  var y = height;
@@ -21,7 +28,8 @@ function draw() {
     } else {
       x = 0;
     }
-    
+   fill(color(rVal,gVal,bVal));
+   stroke(color(rVal,gVal,bVal));
     while (x <= width) {
       ellipse(x, y, circleDiameter, circleDiameter);
       x = x + circleDiameter;
@@ -29,5 +37,9 @@ function draw() {
 
     y = y - circleRadius;
     isShifted = !isShifted;
+    
+    rVal = rVal - 2;
+    gVal = gVal + 7;
+    bVal = bVal + 3;
   }
 }
